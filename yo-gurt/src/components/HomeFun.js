67d8600028d7c52fdbc4e48f5dcbaa1822ab3703
David S <dshi2008mayo@gmail.com>
2026-02-02 17:22:22 -0500

    com
    com

--- yo-gurt/src/components/HomeFun.js
+++ yo-gurt/src/components/HomeFun.js
@@ -34,13 +34,13 @@ function HomeFun() {
                     {renderCard({
                         src:'/images/arjun.jpg', 
                         label:'Arjun Nair',
-                        text:"Arjun Nair is a junior at Andover High School. He has over 10 years of chess experience and has taught 125+ students both locally and internationally. He specializes in teaching young beginners and runs numerous volunteer chess programs.",
+                        text:"Arjun Nair is a senior at Andover High School. He has over 10 years of chess experience and has taught 125+ students both locally and internationally. He specializes in teaching young beginners and runs numerous volunteer chess programs.",
                         path:'mailto:arjun.nair610@gmail.com'
                     })}
                     {renderCard({
                         src:'/images/victor.jpg', 
                         label:'Victor Feng',
-                        text:'Victor Feng is a junior at Lexington High School. He captained LHS to first place at states in 2023 and 2025, top 10 chess teams in US High School Championships in 2023 and 2025, won US Amateurs Scholastics twice, and co-champed the 2025 Spiegel Cup.',
+                        text:'Victor Feng is a senior at Lexington High School. He captained LHS to first place at states in 2023 and 2025, top 10 chess teams in US High School Championships in 2023 and 2025, won US Amateurs Scholastics twice, and co-champed the 2025 Spiegel Cup.',
                         path:'mailto:vfeng08@gmail.com'
                     })}
                 </ul>
@@ -48,13 +48,13 @@ function HomeFun() {
                     {renderCard({
                         src:'/images/eric.jpg', 
                         label:'Eric Feng',
-                        text:"Eric Feng is a sophomore at Acton Boxborough High School. He is the second most decorated chess player in Massachusetts History, winning the esteemed Spiegel Cup 5 times and winning US Amateurs Scholastics twice. ",
+                        text:"Eric Feng is a junior at Acton Boxborough High School. He is the second most decorated chess player in Massachusetts History, winning the esteemed Spiegel Cup 5 times and winning US Amateurs Scholastics twice. ",
                         path:'mailto:eric.feng1003@gmail.com'
                     })}
                     {renderCard({
                         src:'/images/raymond.png',
                         label:'Raymond Xu',
-                        text:'Raymond Xu is a junior at Lexington High School. He won the 8th grade national champion in 2022, and has won multiple Spiegel cups at the youth level. He won the U.S. Amateur top scholastic award in 2023, and he has taught over 1000 hours of chess.',
+                        text:'Raymond Xu is a senior at Lexington High School. He won the 8th grade national champion in 2022, and has won multiple Spiegel cups at the youth level. He won the U.S. Amateur top scholastic award in 2023, and he has taught over 1000 hours of chess.',
                         path:'mailto:rayxu66@gmail.com'
                     })}
                 </ul>
@@ -76,13 +76,20 @@ function HomeFun() {
                         text:"Ethan Zhou is a junior at Lexington High School. He is a model, actor, and social media influencer. He is responsible for all our social media engagement. If you are looking to collab, please email him!!!",
                         path:'mailto:ethanzhou878@gmail.com'
                     })}
+                </ul>
+                <ul className='cards__items'>
                     {renderCard({
-                        src: '/images/dylan.png', 
+                        src: '/images/dylan.png',
                         label: 'Dylan Ho',
                         text: " Dylan Ho is a senior at Andover High School. He is graphic designer social media influencer with more than a thousand followers. He attained thousands of views on posts and collaborated with other influencers. ",
                         path:'mailto:dillho1207@gmail.com'
                     })}
-  
+                    {renderCard({
+                        src: '/images/david.png',
+                        label: 'David Shi',
+                        text: "David is a senior at Andover High School. He coded this website and works a lot with machine learning. Email him with reports of any technical bugs.",
+                        path:'mailto:dshi2008mayo@gmail.com'
+                    })}
                 </ul>
             </div>
         </div>
@@ -100,6 +107,12 @@ function HomeFun() {
                         src:'/images/aziz.png', 
                         label:'Aziz Bekanvarov',
                         text:"Aziz has been playing chess for 8+ years. He love teaching kids how to think during a game, spot tactics, and build confidence no matter if they are a beginner, already competing at tournaments or someone who plays chess just to enjoy it. He is really excited to meet new students and wants to enjoy teaching chess to everyone!",
+                        path:'mailto:azizbekanvarov2010@gmail.com'
+                    })}
+                    {renderCard({
+                        src:'/images/johnathon.png', 
+                        label:'Jonathon Yu',
+                        text:"Johnathon Yu is a junior at Chelmsford High School. He focuses primarily on creating statewide chess opportunities for students of all levels. He has over 5 years of chess experience, is a skilled and active tournament player with a solid, strategic style, and specializes in analyzing games and teaching fundamentals to help students improve.",
                         path:'mailto:azizbekanvarov2010@gmail.com'
                     })}
                 </ul>
